ab85a4e462280c621b4ea0bc0fed255d8d2e5691
sanatan0105 <sanatan0105@gmail.com>
2018-08-09 16:59:16 +0530

    Another commit

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,28 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
+    assert_select "title", "Some Random title"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
+    assert_select "title", "Help || Some Random title"
   end
 
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "About || Some Random title"
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact || Some Random title"
+  end
 end
